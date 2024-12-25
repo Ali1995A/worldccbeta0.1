a9919d0cc8866e379885c67db773ff618ae6b7b4
Ali1995A <107311717+Ali1995A@users.noreply.github.com>
2024-12-25 16:47:15 +0800

    Update easter-egg.js

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -25,7 +25,7 @@ class EasterEgg {
 
         const cc = document.createElement('div');
         cc.className = 'easter-egg';
-        cc.textContent = 'CC';
+        cc.textContent = 'CC Merry Christmas!';
         
         // 随机样式
         const size = this.getRandomValue(this.config.minSize, this.config.maxSize);
