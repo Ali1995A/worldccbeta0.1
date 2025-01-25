831225652e8306e34c8e855683f13ea43e10d154
Ali1995A <107311717+Ali1995A@users.noreply.github.com>
2025-01-25 14:39:51 +0800

    Update easter-egg.js

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -25,7 +25,7 @@ class EasterEgg {
 
         const cc = document.createElement('div');
         cc.className = 'easter-egg';
-        cc.textContent = 'CC Happy 2025 Year!';
+        cc.textContent = 'Happy Chinese New Year 2025, my CC!';
         
         // 随机样式
         const size = this.getRandomValue(this.config.minSize, this.config.maxSize);
