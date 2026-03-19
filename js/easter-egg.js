cc84f34266a22a98d0f112c9bfa051a1b1b66323
Ali1995A <liexpress@gmail.com>
2026-03-19 14:09:41 +0800

    Update easter egg copy

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -25,7 +25,7 @@ class EasterEgg {
 
         const cc = document.createElement('div');
         cc.className = 'easter-egg';
-        cc.textContent = 'HELLO , MY CC!';
+        cc.textContent = 'CC，超可爱！';
         
         // 随机样式
         const size = this.getRandomValue(this.config.minSize, this.config.maxSize);
